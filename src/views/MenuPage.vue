95216919a094dfd6e8185ff52ebaeaf7e156743d
ZhangTong <ztt446810938@gmail.com>
2023-11-16 23:04:49 +0000

    commit 2

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -2,27 +2,30 @@
 
 <template>
   <div id="background">
-    <h1>Menu Page</h1>
-    <ul>
-      <li v-for="item in menuItems" :key="item.id">{{ item.name }} - {{ item.price }}</li>
-    </ul>
+    <div id="PageName"> DWTE menupage</div>
+    <div id="bottons">
+        <el-dropdown :hide-on-click="false">
+  <span class="el-dropdown-link">
+    Recipes<i class="el-icon-arrow-down el-icon--right"></i>
+  </span>
+  <el-dropdown-menu slot="dropdown">
+    <el-dropdown-item>Indian Food</el-dropdown-item>
+    <el-dropdown-item>Nigerian Food</el-dropdown-item>
+    <el-dropdown-item>Chinese Food</el-dropdown-item>
+  </el-dropdown-menu>
+</el-dropdown>
+    </div>
+    <div id="logo1">
+        <i class="el-icon-burger"></i>
+    <div id="logo2">
+        <i class="el-icon-potato-strips"></i>
+    </div>
+    <div id="logo3">
+        <i class="el-icon-ice-tea"></i>
+    </div>  
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      menuItems: [
-        { id: 1, name: 'Item 1', price: '$10' },
-        { id: 2, name: 'Item 2', price: '$15' },
-        // Add more menu items as needed
-      ],
-    };
-  },
-};
-</script>
-
 <style scoped>
 #background{
 position: fixed;
@@ -32,8 +35,8 @@ top: 0%;
 left: 0%;
 background-image: linear-gradient(
     90deg,
-    LightPink,
-    Black
+    rgb(250, 125, 143),
+    rgb(249, 244, 244)
 );
 background-size: 400%;
 animation: mynimation 10s infinite;
@@ -48,5 +51,55 @@ animation: mynimation 10s infinite;
    100%{
     background-position: 0% 50%;
    }
+}
+#PageName{
+    color: black;
+    font-size:65px;
+    font-weight: 800;
+    position: absolute;
+    top:25%;
+    left: 10%;
+}
+#bottons{
+     position: absolute;
+     top:50%;
+     left:20%;
+}
+.el-dropdown-link{
+    color:black;
+    font-size: 40px;
+    font-weight: 900; 
+}
+.el-dropdown-menu{
+    color:black;
+    font-size: 50px;
+    font-weight: 900;
+    width: 150px;
+    background-color: rgba(255, 255, 2555, 0.4);
+}
+#logo1{
+    position: fixed;
+     top:85%;
+     left:35%;
+}
+#logo2{
+    position: fixed;
+     top:85%;
+     left:45%;
+}
+#logo3{
+    position: fixed;
+     top:85%;
+     left:55%;
+}
+.el-icon-burger{
+    font-size: 95px;
+    left: 30%;
+}
+.el-icon-potato-strips{
+    font-size: 95px;
+}
+.el-icon-ice-tea{
+    font-size: 95px;
 }
 </style>
